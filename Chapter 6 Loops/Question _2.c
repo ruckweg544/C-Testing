8588e17e37637253dea497cd221c194c9065c999
THX-1138 <139034465+ruckweg544@users.noreply.github.com>
2025-05-17 07:11:31 +0000

    Question 3 completed

--- Chapter 6 Loops/Question _2.c
+++ Chapter 6 Loops/Question _2.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-    int num_1, num_2, big, small;
+    int num_1, num_2, big, small, tmp;
     printf("Enter two integers: ");
     scanf("%d %d", &num_1, &num_2);
-    num_1 > num_2 ? big = num_1, small = num_2 : big = num_2, small = num_1;
+    num_1 > num_2 ? (big = num_1, small = num_2) : (big = num_2, small = num_1);
     
+    while (small != 0) {
+        tmp = small;
+        small = big % small;
+        big = tmp;
+    }
+    printf("Greatest common divisor: %d\n", big);
+    return 0;
 }
---
+++ Chapter 6 Loops/Question_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int m, n, big, small, tmp;
+    printf("Enter a fraction: ");
+    scanf("%d/%d", &m, &n);
+    m > n ? (big = m, small = n) : (big = n, small = m);
+    
+    while (small != 0) {
+        tmp = small;
+        small = big % small;
+        big = tmp;
+    }
+    printf("In lowest terms: %d/%d\n", m/big, n/big);
+    return 0;
+}

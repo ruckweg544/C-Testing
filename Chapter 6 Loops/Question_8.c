47dee0dbb8949cdfa8fc349e9349e38166a6ad02
THX-1138 <139034465+ruckweg544@users.noreply.github.com>
2025-05-17 19:27:41 +0900

    Update Question_8.c

--- Chapter 6 Loops/Question_8.c
+++ Chapter 6 Loops/Question_8.c
@@ -1,3 +1,6 @@
+/* The program prints a one-month calendar. */
+// The user specifies the number of days in the month and the day of the week on which the month begins
+
 #include<stdio.h>
 
 int main(void) {
